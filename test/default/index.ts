ec1459d8dd910a028cd7d9dae469a67fb7e4f1ce
mathcovax <campani.mathieu@gmail.com>
2023-11-03 16:51:43 +0000

    [feat] testing

---
+++ test/default/index.ts
@@ -0,0 +1,22 @@
+import {zod} from "@duplojs/duplojs";
+import {workerTesting} from "@duplojs/worker-testing";
+import {readFileSync} from "fs";
+
+export default workerTesting(
+	__dirname + "/route.ts",
+	[
+		{
+			title: "get html file",
+			url: "http://localhost:1506/user.html",
+			method: "GET",
+			response: {
+				code: 200,
+				body: zod.literal(readFileSync(__dirname + "/../routes/user.html", "utf-8"))
+			}
+		}
+	],
+	[
+		"/user/{id}",
+		"/user",
+	]
+);
---
+++ test/default/route.ts
@@ -0,0 +1,14 @@
+import Duplo from "@duplojs/duplojs";
+import duploRoutesDirectory, {matchHtmlOrCSSFile, matchScriptFile} from "../../scripts/routesDirectory";
+import {parentPort} from "worker_threads";
+
+const duplo = Duplo({port: 1506, host: "0.0.0.0"});
+
+duplo.use(duploRoutesDirectory, {
+	path: __dirname + "/../routes", 
+	matchs: [
+		matchHtmlOrCSSFile, 
+		matchScriptFile
+	]
+})
+.then(() => duplo.launch(() => parentPort?.postMessage("ready")));
